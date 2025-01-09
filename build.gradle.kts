de7cc3ca51c2c0eab8036f31eb38cc55c90c5877
george <geomel@yandex.ru>
2025-01-09 14:01:04 +0300

    add Spring boot project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+	application
+	id("org.springframework.boot") version "3.3.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+repositories {
+	mavenCentral()
+}
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
